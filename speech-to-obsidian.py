9d0d3e1bead37d0f6f97c7f589b48d20002488af
Aidan Astridge <115724380+aidanastridge@users.noreply.github.com>
2025-02-13 15:49:28 -0500

    Update speech-to-obsidian.py

--- speech-to-obsidian.py
+++ speech-to-obsidian.py
@@ -7,5 +7,9 @@ today = datetime.now()
 # Format the date in YMD format
 ymd_format = today.strftime("%Y-%m-%d")
 
+from markitdown import MarkItDown
 
+md = MarkItDown(enable_plugins=False) # Set to True to enable plugins
+result = md.convert("transcription.docx")
+print(result.text_content)
 
