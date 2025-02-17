00aefa0d3b7c2b211b6a0e9d8373bf61904fbed6
Aidan Astridge <115724380+aidanastridge@users.noreply.github.com>
2025-02-17 00:10:06 -0500

    Update speech-to-obsidian.py

--- speech-to-obsidian.py
+++ speech-to-obsidian.py
@@ -1,12 +1,17 @@
+# Libraries
 import sounddevice as sd
 import numpy as np
 import wavio
 import datetime
+import whisper
+from pathlib import Path
+import json
 
+# Paths
 recording = 'output.wav'
 
 # Parameters
-duration = 5  # seconds
+duration = 10  # seconds
 sample_rate = 44100  # Hz
 
 # Record audio
@@ -16,4 +21,13 @@ sd.wait()  # Wait until recording is finished
 print("Recording complete.")
 
 wavio.write(recording, audio_data, sample_rate, sampwidth=2)
+
+model = whisper.load_model('tiny')
+path = Path(recording)
+
+result = model.transcribe(str(path), language='en', verbose=True)
+
+# Dump the results to a JSON file
+with open('transcript.json', "w") as file:
+    json.dump(result['text'], file, indent=4)
 
