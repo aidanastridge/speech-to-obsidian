29c66da9d54bb399a5bc606ff3cc23a2c600c515
Aidan Astridge <115724380+aidanastridge@users.noreply.github.com>
2025-02-17 16:24:29 -0500

    Update speech-to-obsidian.py

--- speech-to-obsidian.py
+++ speech-to-obsidian.py
@@ -6,6 +6,10 @@ import datetime
 import whisper
 from pathlib import Path
 import json
+from markitdown import MarkItDown
+
+# Get today's date
+today_date = datetime.datetime.now().strftime('%Y-%m-%d')
 
 # Paths
 recording = 'output.wav'
@@ -30,4 +34,10 @@ result = model.transcribe(str(path), language='en', verbose=True)
 # Dump the results to a JSON file
 with open('transcript.json', "w") as file:
     json.dump(result['text'], file, indent=4)
+
+md = MarkItDown() # Set to True to enable plugins
+result = md.convert('transcript.json')
+
+with open(f'{today_date}.md', 'w') as f:
+    f.write(result.text_content)
 
