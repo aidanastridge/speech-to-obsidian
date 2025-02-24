e958c1cee58467cceadb3341dad73a595714cb73
Aidan Astridge <115724380+aidanastridge@users.noreply.github.com>
2025-02-24 15:35:05 -0500

    Update speech-to-obsidian.py

--- speech-to-obsidian.py
+++ speech-to-obsidian.py
@@ -1,17 +1,13 @@
-# Libraries
 import sounddevice as sd
 import wavio
 import datetime
 import whisper
-import json
-from markitdown import MarkItDown
 
 # Constants
-RECORDING_DURATION = 80  # seconds
+RECORDING_DURATION = 40  # seconds
 SAMPLE_RATE = 44100  # Hz
-RECORDING_PATH = 'output.wav'
-TRANSCRIPT_PATH = 'transcript.json'
-DAILY_NOTES_PATH = '/Daily notes'
+RECORDING_PATH = '/Users/aidanastridge/Documents/recordings/output.wav'
+DAILY_NOTES_PATH = '/Users/aidanastridge/Library/CloudStorage/GoogleDrive-astridgeaidan@gmail.com/My Drive/Public/Daily notes'
 
 # Get today's date
 today_date = datetime.datetime.now().strftime('%Y-%m-%d')
@@ -28,15 +24,16 @@ wavio.write(RECORDING_PATH, audio_data, SAMPLE_RATE, sampwidth=2)
 model = whisper.load_model('small')
 result = model.transcribe(RECORDING_PATH, language='en', verbose=True)
 
-# Save transcript to JSON
-with open(TRANSCRIPT_PATH, "w") as file:
-    json.dump(result['text'], file, indent=4)
+# Create Markdown content
+markdown_content = f"""
+Tags: #tasks
 
-# Convert transcript to Markdown
-md = MarkItDown()  # Set to True to enable plugins, having errors when parameter is filled
-result = md.convert(TRANSCRIPT_PATH)
+{result['text']}
+"""
 
 # Save Markdown to daily notes file
 with open(f'{DAILY_NOTES_PATH}/{today_date}.md', 'w') as f:
-    f.write(result.text_content)
+    f.write(markdown_content)
+
+print("Markdown file saved successfully!")
 
